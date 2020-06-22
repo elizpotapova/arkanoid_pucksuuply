b3ed88f13013e88b0ec3b931124293f2f79b19e7
elizpotapova <66255970+elizpotapova@users.noreply.github.com>
2020-06-22 20:46:27 +0300

    sup1

---
+++ PuckSupply.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "PuckSupply.h"
+#include <iostream>
+using namespace std;
+
+
+PuckSupply::PuckSupply(int HP)
+{
+	_Puck_arr = new Puck[HP];
+	_cnt = HP;
+}
+
+
+
---
+++ PuckSupply.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+#include "Puck.h"
+class PuckSupply
+{
+public:
+	PuckSupply(int HP);
+	Puck getPuck();
+	~PuckSupply();
+	friend class GameTable;
+	friend void GameRun();
+
+};
+
